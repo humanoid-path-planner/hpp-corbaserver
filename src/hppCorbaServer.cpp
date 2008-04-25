42675dd9bf6efbcb6b926f259e22782464490f99
florent <florent@dhcpt1253.a02.aist.go.jp>
2008-04-25 15:41:51 +0900

    Added missing file.

---
+++ src/hppCorbaServer.cpp
@@ -0,0 +1,29 @@
+#include "hppCorbaServer/hppciServer.h"
+#include "KineoModel/kppLicense.h"
+
+#if DEBUG==2
+#define ODEBUG2(x) std::cout << "hppCorbaServer:" << x << std::endl
+#define ODEBUG1(x) std::cerr << "hppCorbaServer:" << x << std::endl
+#elif DEBUG==1
+#define ODEBUG2(x)
+#define ODEBUG1(x) std::cerr << "hppCorbaServer:" << x << std::endl
+#else
+#define ODEBUG2(x)
+#define ODEBUG1(x)
+#endif
+
+int main(int argc, char** argv)
+{
+  if (CkppLicense::initialize() != true) {
+    ODEBUG1(" failed to get a Kineo license");
+    exit(-1);
+  }
+  ChppPlanner* hppPlanner = new ChppPlanner;
+  ChppciServer server(hppPlanner, argc, argv);
+
+  if (server.startCorbaServer() == -1){
+    ODEBUG1(" failed to start CORBA server.");
+    exit(-1);
+  }
+  server.processRequest(true);
+}
