0278fe42dec732c8bdfab272fa087aabb2b47fff
Florent Lamiraux <florent@laas.fr>
2014-03-31 14:33:16 +0200

    Add documentation about how to use corba server.

--- include/hpp/corbaserver/doc.hh
+++ include/hpp/corbaserver/doc.hh
@@ -11,8 +11,19 @@ Three main Corba interfaces are implemented:
 However, the main interface classes for users of this package are
 \li hpp::corbaServer::Server that implements the above Corba interfaces
 
-\section hppCorbaServer_sec_howto How to use this package
+\section hppCorbaServer_sec_howto How to communicate with the CORBA server
 
-To use this package, See documentation of class hpp::corbaServer::Server.
+The easiest way is
+\li to launch hppcorbaserver executable or any executable that implements the server,
+\li open a python terminal and type:
+\code
+from hpp.corbaserver import Client
+cl = Client ()
+\endcode
+Then variable \c cl contains three members "robot", "obstacle" and problem that can send request to the server.
+
+\section hppCorbaServer_sec_embedding How to embed a server in an application
+
+To embed a CORBA server in an application, See documentation of class hpp::corbaServer::Server.
 
 */
