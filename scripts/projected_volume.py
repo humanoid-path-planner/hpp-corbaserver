5bd3ed0bccedc872dbe069e1c4325313550e71c9
Andreas Orthey <andreas.orthey@gmx.de>
2014-05-13 17:12:05 +0200

    more efficient cvx hull, correct rviz display

--- scripts/projected_volume.py
+++ scripts/projected_volume.py
@@ -20,35 +20,38 @@ class ProjectedVolume ():
                 self.q1 = [0.0, 0.0, 0.705, 1.0, 0., 0., 0.0, 0.0, 0.0, 0.0, 0.0, -0.4, 0, -1.2, -1.0, 0.0, 0.0, 0.174532, -0.174532, 0.174532, -0.174532, 0.174532, -0.174532, 0.261799, -0.17453, 0.0, -0.523599, 0.0, 0.0, 0.174532, -0.174532, 0.174532, -0.174532, 0.174532, -0.174532, 0.0, 0.0, -0.453786, 0.872665, -0.418879, 0.0, 0.0, 0.0, -0.453786, 0.872665, -0.418879, 0.0]
                 self.q2 = [0.0, 0.0, 0.705, 1, 0, 0, 0, 0.0, 0.0, 0.0, 0.0, 1.0, 0, -1.4, -1.0, 0.0, 0.0, 0.174532, -0.174532, 0.174532, -0.174532, 0.174532, -0.174532, 0.261799, -0.17453, 0.0, -0.523599, 0.0, 0.0, 0.174532, -0.174532, 0.174532, -0.174532, 0.174532, -0.174532, 0.0, 0.0, -0.453786, 0.872665, -0.418879, 0.0, 0.0, 0.0, -0.453786, 0.872665, -0.418879, 0.0]
                 self.q = self.q0
+                self.distanceToProjection = 1
 
         def setConfig(self,q):
-                self.robot.setCurrentConfig(q)
+                self.q = q
+                self.robot.setCurrentConfig(self.q)
 
         def setRandomConfig(self):
                 self.robot.setCurrentConfig(qrand)
 
         def computeConvexHull(self):
                 from scipy.spatial.qhull import Delaunay
+                from convex_hull import convex_hull
 
                 #self.idx = numpy.concatenate([self.hull[:,0:1],self.hull[:,2:3]],1)
                 #use y,z coordinates only
                 self.cap2d = numpy.concatenate([self.capsule[:,1:2],self.capsule[:,2:3]],1) 
-                self.idx = Delaunay(self.cap2d).convex_hull
+
+                #make cap2d hashable
+                self.cap2dhash = map(tuple, self.cap2d)
+                self.hull = convex_hull(self.cap2dhash)
 
                 r = rospy.Rate(1)
                 r.sleep()
-                for i in range(0,len(self.idx),1):
-                        yh = self.capsule[self.idx[i,0],1]
-                        zh = self.capsule[self.idx[i,1],2]
-                        print yh,zh
-                        self.scene_publisher.addSphere(0.5,
-                                        yh,
-                                        zh,
-                                        0.1,
-                                        0.1,
-                                        0.1
-                                        )
+                #for i in range(0,len(self.hull),1):
+                #        self.scene_publisher.addSphere(0.5,
+                #                        self.hull[i][0],
+                #                        self.hull[i][1],
+                #                        0.5,
+                #                        0.01,
+                #                        0.01)
 
+                self.scene_publisher.addPolygon(self.distanceToProjection, self.hull)
                 self.scene_publisher.publishObjects()
                 r.sleep()
                 r.sleep()
@@ -58,10 +61,9 @@ class ProjectedVolume ():
                 #access 3 elements at a time (x,y,z)
                 self.capsule = numpy.empty((len(capsulePos)/5, 5))
                 ctr = 0
-                scale = 0.5
                 for i in range(0,len(capsulePos),5):
                         #capsule containts x,y,z,radius,length
-                        self.capsule[ctr] = scale*numpy.array(capsulePos[i:i+5])
+                        self.capsule[ctr] = numpy.array(capsulePos[i:i+5])
                         ctr+=1
 
         def display(self):
@@ -69,7 +71,7 @@ class ProjectedVolume ():
                 r.sleep()
                 self.scene_publisher(self.q)
                 for i in range(0,len(self.capsule),1):
-                        self.scene_publisher.addSphere(0.5,
+                        self.scene_publisher.addSphere(self.distanceToProjection,
                                         self.capsule[i][1],
                                         self.capsule[i][2],
                                         0.001,
