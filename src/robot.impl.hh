fe72be15914cb3f9178ba28f7e6558c5839c01eb
Andreas Orthey <andreas.orthey@gmx.de>
2014-05-26 14:21:42 +0200

    Merged changes from nassimeblinlaas

--- src/robot.impl.hh
+++ src/robot.impl.hh
@@ -164,23 +164,6 @@ namespace hpp
 	  throw (hpp::Error);
 
 
-	virtual hpp::floatSeq* computeVolume () throw (hpp::Error);
-
-	virtual hpp::floatSeq* parseCapsulePoints () throw (hpp::Error);
-
-	virtual hpp::floatSeq* projectConfigurationUntilIrreducible 
-          (const hpp::floatSeq& dofArray) throw (hpp::Error);
-
-        virtual hpp::floatSeq* gradientConfigurationWrtProjection 
-          (const hpp::floatSeq& dofArray) throw (hpp::Error);
-
-        virtual hpp::floatSeq* gradientConfigurationWrtProjection () 
-                throw (hpp::Error);
-
-        virtual hpp::floatSeq* getRandomConfig() 
-                throw (hpp::Error);
-
-
       private:
 	typedef std::map <std::string, JointPtr_t> JointMap_t;
 	// Store devices, joints and bodies in construction.
