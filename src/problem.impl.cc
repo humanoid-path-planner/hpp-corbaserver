85dc260d3ab0df2622fa2b743fa7cdbef15205cf
Florent Lamiraux <florent@laas.fr>
2015-08-28 09:09:10 +0200

    In Problem::directPath, throw if direct path is not valid.
      - use non deprecated validate method.

--- src/problem.impl.cc
+++ src/problem.impl.cc
@@ -890,16 +890,19 @@ namespace hpp
 	  }
 	  SteeringMethodPtr_t sm = problemSolver_->problem ()->steeringMethod ();
 	  PathPtr_t dp = (*sm) (*start, *end);
+	  PathPtr_t unused;
+	  PathValidationReportPtr_t report;
+	  if (!problemSolver_->problem()->pathValidation ()->validate
+	      (dp, false, unused, report)) {
+	    std::ostringstream oss; oss << *report;
+	    throw hpp::Error (oss.str ().c_str ());
+	  }
 	  // Add Path in problem
 	  PathVectorPtr_t path
 	    (core::PathVector::create (dp->outputSize (),
 				       dp->outputDerivativeSize ()));
 	  path->appendPath (dp);
 	  problemSolver_->addPath (path);
-	  PathPtr_t unused;
-	  PathValidationReportPtr_t report;
-	  problemSolver_->problem()->pathValidation ()->validate
-	    (dp, false, unused, report);
 	} catch (const std::exception& exc) {
 	  throw hpp::Error (exc.what ());
 	}
