7cfe0d9a9349c2376ae747d34405beef0f81350c
Joseph Mirabel <jmirabel@laas.fr>
2015-12-11 17:49:36 +0100

    Add SteeringMethod to getAvailable

--- src/problem.impl.cc
+++ src/problem.impl.cc
@@ -168,11 +168,13 @@ namespace hpp
           ret = problemSolver_->getKeys <core::ConfigurationShooterBuilder_t, Ret_t> ();
         } else if (w == "pathvalidation") {
           ret = problemSolver_->getKeys <core::PathValidationBuilder_t, Ret_t> ();
+        } else if (w == "steeringmethod") {
+          ret = problemSolver_->getKeys <core::SteeringMethodBuilder_t, Ret_t> ();
         } else if (w == "numericalconstraint") {
           ret = problemSolver_->getKeys <core::NumericalConstraintPtr_t, Ret_t> ();
         } else if (w == "type") {
           ret = boost::assign::list_of ("PathOptimizer") ("PathProjector")
-            ("PathPlanner") ("ConfigurationShooter")
+            ("PathPlanner") ("ConfigurationShooter") ("SteeringMethod")
             ("PathValidation") ("NumericalConstraint");
         } else {
           throw Error ("Type not understood");
