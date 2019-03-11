31ca707a93cbb3cd6531613ed747d132be6e9bbb
Joseph Mirabel <jmirabel@laas.fr>
2019-03-11 19:31:27 +0100

    Update example script.

--- tests/check_core_idl.py
+++ tests/check_core_idl.py
@@ -7,19 +7,26 @@ newProblem()
 robot = Robot ("ur5")
 ps = ProblemSolver (robot)
 
-distance = ps.hppcorba.problem.getDistance()
-sm = ps.hppcorba.problem.getSteeringMethod ()
-pv = ps.hppcorba.problem.getPathValidation ()
+# As of now, this isn't safe because
+# it is hard to know when C++ class ProblemSolver
+# deletes the current problem and create a new one.
+problem = ps.hppcorba.problem.getProblem()
+
+distance = problem.getDistance()
+sm = problem.getSteeringMethod ()
+pv = problem.getPathValidation ()
+problem.setDistance (distance) # does nothing because the distance did not change.
 
 tools = Tools()
 
 q0 = robot.getCurrentConfig()
 qr = robot.shootRandomConfig()
-distance.value (q0, qr)
+v1 = distance.value (q0, qr)
 weights = distance.getWeights()
 weights[0] = 0
 distance.setWeights (weights)
-distance.value (q0, qr)
+v2 = distance.value (q0, qr)
+assert v1 != v2
 
 path = sm.call (q0, qr)
 
@@ -29,8 +36,12 @@ path.outputSize()
 
 isValid, validPart, report = pv.validate (path, False)
 
+# Decrease the object ref count of the underlying HPP object
 tools.deleteServantFromObject(distance)
 tools.deleteServantFromObject(path)
 tools.deleteServantFromObject(sm)
+tools.deleteServantFromObject(pv)
+# As of now, there is no ref count on class Problem
+tools.deleteServantFromObject(problem)
 
 tools.shutdown()
