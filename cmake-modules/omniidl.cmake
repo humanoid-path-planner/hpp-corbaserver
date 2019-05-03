798290ff50d1c6428c6b97bbd009df3eaf10ae80
Joseph Mirabel <jmirabel@laas.fr>
2019-05-03 16:26:04 +0200

    Add some documentation

--- cmake-modules/omniidl.cmake
+++ cmake-modules/omniidl.cmake
@@ -1,3 +1,23 @@
+# Copyright (c) 2019, Joseph Mirabel
+# Authors: Joseph Mirabel (joseph.mirabel@laas.fr)
+#
+# This file is part of hpp-core.
+# hpp-core is free software: you can redistribute it
+# and/or modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation, either version
+# 3 of the License, or (at your option) any later version.
+#
+# hpp-core is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Lesser Public License for more details.  You should have
+# received a copy of the GNU Lesser General Public License along with
+# hpp-core. If not, see <http://www.gnu.org/licenses/>.
+
+# Generate servant implementation of the IDL interface for the corresponding HPP class.
+#
+# Assumes the following has been called before:
+# ADD_REQUIRED_DEPENDENCY("hpp-corbaserver")
 MACRO(GENERATE_IDL_CPP_IMPL FILENAME DIRECTORY)
   SET(oneValueArgs HH_SUFFIX HPP_SUFFIX HXX_SUFFIX CC_SUFFIX)
   SET(multiValueArgs ARGUMENTS)
