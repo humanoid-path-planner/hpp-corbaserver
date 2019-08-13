345fb09c0710f6c93a629fc54af476825d7cd76a
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-08-13 16:56:21 +0200

    [Tests] omniNames is in PATH but not necessarily in /usr/bin

--- tests/robot.py
+++ tests/robot.py
@@ -15,9 +15,9 @@ from hpp.corbaserver.robot import Robot
 class Test(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.omni_names = Popen(['/usr/bin/omniNames', '-start'], stdout=sys.stdout)
+        cls.omni_names = Popen(['omniNames', '-start'], stdout=sys.stdout)
         sleep(1)
-        cls.server = Popen(["../src/hppcorbaserver"], stdout=sys.stdout)
+        cls.server = Popen(["../src/hppcorbaserver"], stdout=sys.stdout, env={'ORBInitRef': 'NameService=corbaname::localhost'})
         sleep(1)
         cls.server.poll()
         if cls.server.returncode is not None:
