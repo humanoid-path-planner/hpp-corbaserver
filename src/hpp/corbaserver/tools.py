a470bea97d6097e6545b4a3a39951da410afa54b
Joseph Mirabel <jmirabel@laas.fr>
2019-12-19 10:14:34 +0100

    Fix tools.py.

--- src/hpp/corbaserver/tools.py
+++ src/hpp/corbaserver/tools.py
@@ -1,8 +1,5 @@
 import sys
-from omniORB import CORBA
-import CosNaming, hpp_idl.hpp as _hpp
-
-from .client import CorbaError, _getIIOPurl
+import hpp_idl.hpp as _hpp
 
 def loadServerPlugin (context, plugin, url = None):
     client = Tools (url)
@@ -17,36 +14,13 @@ if sys.version_info.major > 2:
     createContext.__doc__ = _hpp.Tools.createContext__doc__
 
 def Tools(url = None):
-    import sys
-    orb = CORBA.ORB_init (sys.argv, CORBA.ORB_ID)
-    if url is None:
-        ns = orb.string_to_object (_getIIOPurl())
-    else:
-        ns = orb.string_to_object (url)
+    from .client import Client
+    class _ToolClient(Client):
+        def __init__(self, url = None):
+            self._initOrb (url)
 
-    rootContext = ns._narrow(CosNaming.NamingContext)
-    if rootContext is None:
-      raise CorbaError ('failed to narrow the root context')
-
-    serviceName = "hpp.tools"
-    name = [CosNaming.NameComponent ("hpp", "tools"),]
-
-    try:
-      obj = rootContext.resolve (name)
-    except CosNaming.NamingContext.NotFound as ex:
-      raise CorbaError (
-        'failed to find the service ``{0}\'\''.format (serviceName))
-
-    try:
-      client = obj._narrow (_hpp.Tools)
-    except KeyError:
-      raise CorbaError ('invalid service name ``{0}\'\''.format (serviceName))
-
-    if client is None:
-      # This happens when stubs from client and server are not synchronized.
-      raise CorbaError (
-        'failed to narrow client for service named ``{0}\'\''.format
-        (serviceName))
+    _tc = _ToolClient(url=url)
+    client = _tc._tools
 
     def deleteServantFromObject (*args):
         """
