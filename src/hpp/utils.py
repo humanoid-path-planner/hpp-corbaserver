e31d9f58b9563098021ac5b635af1287610a7d3d
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-06-04 21:24:52 +0200

    [Python] add a ServerManager util
    ref https://github.com/humanoid-path-planner/hpp-rbprm-corba/commit/ef9021deff6105f6388d2f14b101675f512aa387
    
    This utility might be usefull in other places in HPP.

---
+++ src/hpp/utils.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2020, CNRS
+# Authors: Guilhem Saurel <guilhem.saurel@laas.fr>
+
+import os
+import subprocess
+import time
+
+
+class ServerManager:
+    """A context to ensure a server is running."""
+    def __init__(self, server):
+        self.server = server
+        subprocess.run(['killall', self.server])
+
+    def __enter__(self):
+        """Run the server in background
+
+        stdout and stderr outputs of the child process are redirected to devnull (hidden).
+        preexec_fn is used to ignore ctrl-c signal send to the main script
+        (otherwise they are forwarded to the child process)
+        """
+        self.process = subprocess.Popen(self.server,
+                                        stdout=subprocess.DEVNULL,
+                                        stderr=subprocess.DEVNULL,
+                                        preexec_fn=os.setpgrp)
+        # give it some time to start
+        time.sleep(3)
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.process.kill()
