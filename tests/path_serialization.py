dac89e599a96d8ac5a5f84c9ced39e4643c5edcf
Joseph Mirabel <jmirabel@eurekarobotics.com>
2023-02-24 00:10:14 +0100

    add basic test for savePath and loadPath

---
+++ tests/path_serialization.py
@@ -0,0 +1,37 @@
+"""Test deleting twice the same object does not throw."""
+import unittest
+
+from hpp.corbaserver import Client
+from hpp.corbaserver.tools import Tools
+from hpp.utils import ServerManager
+
+urdf_string = """
+<robot name="test_robot">
+<link name="base_link"/>
+<link name="child_link"/>
+<joint name="joint" type="revolute">
+  <parent link="base_link"/>
+  <child link="child_link"/>
+  <limit effort="50000" lower="-1" upper="1" velocity="10000"/>
+</joint>
+</robot>
+"""
+filename = "./tmp-path.bin"
+
+class Test(unittest.TestCase):
+    def test_save_then_read_path(self):
+        with ServerManager("../src/hppcorbaserver"):
+            self.client = Client()
+            self.client.robot.loadRobotModelFromString("robot", "anchor", urdf_string, "")
+            ok, pid, msg = self.client.problem.directPath([-0.3,], [0.3], False)
+            assert ok
+            path = self.client.problem.getPath(pid)
+            self.client.problem.savePath(path, filename)
+
+        with ServerManager("../src/hppcorbaserver"):
+            self.client = Client()
+            self.client.robot.loadRobotModelFromString("robot", "anchor", urdf_string, "")
+            path = self.client.problem.readPath(filename)
+
+            assert path.initial() == [-0.3]
+            assert path.end() == [0.3]
