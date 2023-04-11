bf7718e100f30697311b017ee1eb3133b13f3b9d
Joseph Mirabel <jmirabel@eurekarobotics.com>
2023-04-11 21:39:08 +0200

    Expose more function of Device.

--- tests/robot.py
+++ tests/robot.py
@@ -43,6 +43,37 @@ class Test(unittest.TestCase):
             _types = [self.client.robot.getJointType(jn)[n:] for jn in robot.jointNames]
             self.assertListEqual(types, _types)
 
+    def test_pinocchio_device(self):
+        with ServerManager("../src/hppcorbaserver"):
+            self.client = Client()
+            self.client.robot.createRobot("test")
+            self.client.robot.appendJoint(
+                "", "joint", "FreeFlyer", [0, 0, 0, 0, 0, 0, 1]
+            )
+
+            self.assertEqual(self.client.robot.getRobotName(), "test")
+
+            c_problem = self.client.problem.getProblem()
+            c_robot = c_problem.robot()
+
+            assert c_robot.configSize() == 7
+            assert c_robot.numberDof() == 6
+
+            q = [0, 0, 0, 0, 0, 0, 1]
+            v = [0.0] * 6
+            a = [0.0] * 6
+            assert c_robot.getCurrentConfiguration() == q
+            assert c_robot.getCurrentVelocity() == v
+            assert c_robot.getCurrentAcceleration() == a
+            q[0] = 1.0
+            c_robot.setCurrentConfiguration(q)
+            assert c_robot.getCurrentConfiguration() == q
+            c_robot.setCurrentVelocity(v)
+            c_robot.setCurrentAcceleration(a)
+
+            c_robot.controlComputation(1)
+            assert c_robot.computationFlag() == 1
+
 
 if __name__ == "__main__":
     unittest.main()
