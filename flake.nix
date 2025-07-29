f3b5f4d7b8fc74cfeef0738ebfc9240f9affb6bf
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-07-29 15:52:15 +0200

    Nix: update gepetto/nix use

--- flake.nix
+++ flake.nix
@@ -2,52 +2,33 @@
   description = "Corba server for Humanoid Path Planner applications";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/refs/pull/362956/head";
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
+    gepetto.url = "github:gepetto/nix";
+    flake-parts.follows = "gepetto/flake-parts";
+    nixpkgs.follows = "gepetto/nixpkgs";
+    nix-ros-overlay.follows = "gepetto/nix-ros-overlay";
+    systems.follows = "gepetto/systems";
+    treefmt-nix.follows = "gepetto/treefmt-nix";
   };
 
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ];
+      systems = import inputs.systems;
+      imports = [ inputs.gepetto.flakeModule ];
       perSystem =
         {
+          lib,
           pkgs,
           self',
-          system,
           ...
         }:
         {
-          _module.args.pkgs = import inputs.nixpkgs {
-            inherit system;
-            overlays = [
-              (final: prev: {
-                hpp-core = prev.hpp-core.overrideAttrs (super: {
-                  patches = (super.patches or [ ]) ++ [
-                    (final.fetchpatch {
-                      url = "https://github.com/humanoid-path-planner/hpp-core/commit/21c46cdcb870e5f28e95a0b5f6e01099d5f8fefb.patch";
-                      hash = "sha256-NMLUkAY0mrr9ktn42iUGbAvZ8bGi9y39w5gQZiofmtA=";
-                    })
-                  ];
-                });
-              })
-            ];
-          };
-          devShells.default = pkgs.mkShell { inputsFrom = [ self'.packages.default ]; };
           packages = {
             default = self'.packages.hpp-corbaserver;
-            hpp-corbaserver = pkgs.python3Packages.hpp-corbaserver.overrideAttrs (_: {
-              src = pkgs.lib.fileset.toSource {
+            hpp-corbaserver = pkgs.python3Packages.hpp-corbaserver.overrideAttrs {
+              src = lib.fileset.toSource {
                 root = ./.;
-                fileset = pkgs.lib.fileset.unions [
+                fileset = lib.fileset.unions [
                   ./cmake-modules
                   ./CMakeLists.txt
                   ./doc
@@ -58,7 +39,7 @@
                   ./tests
                 ];
               };
-            });
+            };
           };
         };
     };
