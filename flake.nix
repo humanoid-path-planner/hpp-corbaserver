bb67339f46298106bca2694451b76485d8114da0
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-06 17:10:04 +0200

    sync with gepetto/nix template

--- flake.nix
+++ flake.nix
@@ -3,13 +3,8 @@
 
   inputs = {
     gepetto.url = "github:gepetto/nix";
-    gazebros2nix.follows = "gepetto/gazebros2nix";
-    flakoboros.follows = "gepetto/flakoboros";
     flake-parts.follows = "gepetto/flake-parts";
-    nixpkgs.follows = "gepetto/nixpkgs";
-    nix-ros-overlay.follows = "gepetto/nix-ros-overlay";
     systems.follows = "gepetto/systems";
-    treefmt-nix.follows = "gepetto/treefmt-nix";
   };
 
   outputs =
