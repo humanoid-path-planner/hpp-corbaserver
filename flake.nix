76e1f98bc6d81a1a413408c7b7bd5f0423d2547b
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-16 12:21:12 +0200

    sync with gepetto/nix template

--- flake.nix
+++ flake.nix
@@ -1,38 +1,28 @@
 {
   description = "Corba server for Humanoid Path Planner applications";
 
-  inputs = {
-    gepetto.url = "github:gepetto/nix";
-    flake-parts.follows = "gepetto/flake-parts";
-    systems.follows = "gepetto/systems";
-  };
+  inputs.gepetto.url = "github:gepetto/nix";
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+    inputs.gepetto.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
-        systems = import inputs.systems;
-        imports = [
-          inputs.gepetto.flakeModule
-          {
-            flakoboros.overrideAttrs.hpp-corbaserver = _: {
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.unions [
-                  ./cmake-modules
-                  ./CMakeLists.txt
-                  ./doc
-                  ./idl
-                  ./include
-                  ./package.xml
-                  ./src
-                  ./tests
-                ];
-              };
-            };
-          }
-        ];
+        overrideAttrs.hpp-corbaserver = {
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./cmake-modules
+              ./CMakeLists.txt
+              ./doc
+              ./idl
+              ./include
+              ./package.xml
+              ./src
+              ./tests
+            ];
+          };
+        };
       }
     );
 }
